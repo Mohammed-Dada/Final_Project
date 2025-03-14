c462c736d77cdcc6bf37a334da3724fb56254987
Mohammed-Dada <ali037dada.khalifi295@gmail.com>
2025-03-14 16:25:29 +0300

    The first Project

---
+++ lib/apikey/apikey.dart
@@ -0,0 +1,1 @@
+String apikey = "5da47b4f6513fc88eddd1ff233fce5d6";
---
+++ lib/apilinks/allapi.dart
@@ -0,0 +1,6 @@
+import 'package:my_flutter_app/apikey/apikey.dart';
+
+String trendingweekurl =
+    'https://api.themoviedb.org/3/trending/all/week?api_key=$apikey';
+String trendingdayurl =
+    'https://api.themoviedb.org/3/trending/all/day?api_key=$apikey';
